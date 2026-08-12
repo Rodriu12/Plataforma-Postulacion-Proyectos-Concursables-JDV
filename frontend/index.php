b9bfae3783525ccef0c70b8c86910c7d59c8f7ec
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-08-11 23:27:26 -0400

    ADD: Readme y archivos php vacios

---
+++ frontend/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Postulación Proyectos Concursables</title>
+</head>
+<body>
+    
+</body>
+</html>
