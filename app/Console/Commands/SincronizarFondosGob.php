ef84a8aba6fc0cefb3d5c55d19ab61c2028be672
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-09-06 02:50:21 -0300

    ADD: Nueva BD para proyectos del fondogob

---
+++ app/Console/Commands/SincronizarFondosGob.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SincronizarFondosGob extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sincronizar-fondos-gob';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
