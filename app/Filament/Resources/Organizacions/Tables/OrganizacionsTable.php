f34202b8cfd3cadc7cad5ab79b8c2af122d16dc3
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-09-05 23:22:11 -0400

    ADD: configuración final de la tabla Organizaciones.

--- app/Filament/Resources/Organizacions/Tables/OrganizacionsTable.php
+++ app/Filament/Resources/Organizacions/Tables/OrganizacionsTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class OrganizacionsTable
@@ -14,7 +15,22 @@ class OrganizacionsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('nombre')
+                    ->label('Organización')
+                    ->searchable()
+                    ->sortable(),
+                    
+                TextColumn::make('rut_juridico')
+                    ->label('RUT Jurídico')
+                    ->searchable(),
+                    
+                TextColumn::make('sector')
+                    ->label('Sector')
+                    ->searchable(),
+                    
+                TextColumn::make('fecha_constitucion')
+                    ->label('Constitución')
+                    ->date(),
             ])
             ->filters([
                 //
