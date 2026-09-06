53c376eeac023a78ee2e20e142aae70450250403
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-09-05 21:26:39 -0400

    ADD: CRUD para la tabla users

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                TextInput::make('rut')
+                    ->default(null),
+                TextInput::make('phone')
+                    ->tel()
+                    ->default(null),
+                TextInput::make('role')
+                    ->required()
+                    ->default('vecino'),
+                Toggle::make('is_active')
+                    ->required(),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+            ]);
+    }
+}
