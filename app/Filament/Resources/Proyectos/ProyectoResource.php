bf0aef17e36b67cf18226999a0bfad8cb6f00fb3
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-09-13 17:18:11 -0300

    MOD: Nuevo icono para proyectos

--- app/Filament/Resources/Proyectos/ProyectoResource.php
+++ app/Filament/Resources/Proyectos/ProyectoResource.php
@@ -20,7 +20,7 @@ class ProyectoResource extends Resource
 {
     protected static ?string $model = Proyecto::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-clipboard-document-check';
 
     protected static ?string $recordTitleAttribute = 'model Proyecto';
 
