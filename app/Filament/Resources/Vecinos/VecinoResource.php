72f4642ff4a617f731cf6bc9b1437330434271e1
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-09-15 13:20:57 -0300

    MOD: Nuevo icono para vecinos

--- app/Filament/Resources/Vecinos/VecinoResource.php
+++ app/Filament/Resources/Vecinos/VecinoResource.php
@@ -18,7 +18,7 @@ class VecinoResource extends Resource
 {
     protected static ?string $model = Vecino::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-user';
 
     protected static ?string $recordTitleAttribute = 'model-vecino';
 
